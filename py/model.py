2dc3162778e8ef6b0d10ceefe38467ada8f453cd
PinkMe <code.hotpiink@gmail.com>
2018-12-01 17:25:13 -0800

    complete parks class in model.py

--- py/model.py
+++ py/model.py
@@ -4,3 +4,25 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class Parks():
+	""" Information on each national park """
+	
+	__tablename__ = "parks"
+	
+	park_id = db.Column(db.Integer, 
+						autoincrement=True,
+						primary_key=True)
+	park_name = db.Column(db.String(50))
+	park_address = db.Column(db.String(50))
+	park_state = db.Column(db.String(2))
+	longitude = db.Column(db.Numeric(10, 8))
+	latitude = db.Column(db.Numeric(10, 8))
+	phone_num = db.Column(db.String(10))
+	park_picture = db.Column(db.String(100))
+	park_website = db.Column(db.String(100))
+
+	def __repr__(self):
+		""" Park display """
+		return(f"Park ID: {self.park_id}\nPark Name: {self.park_name}\nPark State: {self.park_state}")
+
+
