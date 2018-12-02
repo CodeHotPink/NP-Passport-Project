315d112cff06fe7117d232140cdc094002a9d976
PinkMe <code.hotpiink@gmail.com>
2018-12-01 17:36:23 -0800

    complete users class in model.py

--- py/model.py
+++ py/model.py
@@ -18,11 +18,32 @@ class Parks():
 	longitude = db.Column(db.Numeric(10, 8))
 	latitude = db.Column(db.Numeric(10, 8))
 	phone_num = db.Column(db.String(10))
-	park_picture = db.Column(db.String(100))
+	park_photo = db.Column(db.String(100))
 	park_website = db.Column(db.String(100))
 
 	def __repr__(self):
 		""" Park display """
 		return(f"Park ID: {self.park_id}\nPark Name: {self.park_name}\nPark State: {self.park_state}")
 
+class Users():
+	""" User information """
+
+	__tablename__ = "users"
+
+	user_id = db.Column(db.Integer, 
+						autoincrement=True,
+						primary_key=True)
+	first_name = db.Column(db.String(50))
+	last_name = db.Column(db.String(50))
+	gender = db.Column(db.String(3))
+	birthday = db.Column(db.TimeDate)
+	city = db.Column(db.String(50))
+	state = db.Column(db.String(2))
+	email = db.Column(db.String(100))
+	password = db.Column(db.String(28))
+	photo = db.Column(db.String(100))
+
+	def __repr__(self):
+		""" User display """
+		return(f"Name: {self.first_name}\nState: {self.state}\nEmail: {self.email}")
 
