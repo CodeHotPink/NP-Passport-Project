da35be1c88051f1d836f4b903f3c28ea1b42c257
PinkMe <code.hotpiink@gmail.com>
2019-01-10 17:11:17 -0800

    server.py connected to db. not sure if connected to React yet, not displaying react components. server.py is working & is ready for more routes

--- server.py
+++ server.py
@@ -1,18 +1,29 @@
 from model import (Park, User, Visit, Review, connect_to_db, db)
-from flask import (Flask, render_template, session)
-from flask_cors import CORS
+from flask import (Flask, jsonify, redirect, request, session)
+from flask_cors import CORS, cross_origin
+
 # pip3 isntall -u flask-cors
-app = Flask(__name__, template_folder="np_passport/public")
-CORS(app)
+app = Flask(__name__)
+# CORS(app)
+cors = CORS(app, resources={r"/*": { r"supports_credentials":True, r"origins": r"http://localhost:3000" }})
 
 app.secret_key = "Don't go without your passport!"
+
+@app.after_request
+def after(response):
+  """Adds headers to all responses to satisfy CORS."""
+  response.headers.add('Access-Control-Allow-Credentials', 'true')
+  return response
 
 @app.route("/")
 def index():
 	""" index.html for jsx """
-	html = render_template("np_passport/public/index.html")
-	return(html)
+	# html = render_template("np_passport/public/index.html")
+	welcome = "Hi there" 
+	return jsonify(welcome)
 
 if __name__ == "__main__":
 	# connect_to_db(app)
-	app.run()
+	app.debug = True
+	connect_to_db(app, "np_project")
+	app.run(port=5000, host='0.0.0.0')
